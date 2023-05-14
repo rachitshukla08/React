f84848ea9cb7f1011aa82fb634c9657a893abb3e
rachitshukla08 <rachitshukla08@gmail.com>
2023-05-14 15:58:58 +0530

    Basic react food application using swiggy api response

---
+++ CH3/code/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = <h1> Namaste React</h1>;
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
