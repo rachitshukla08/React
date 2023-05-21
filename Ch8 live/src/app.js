61dafc6f2edf87c8d74d186913938cc00c9b2673
rachitshukla08 <rachitshukla08@gmail.com>
2023-05-21 17:43:36 +0530

    Added ch8 live

---
+++ Ch8 live/src/app.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import About from "./components/About";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import ErrorPage from "./components/ErrorPage";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
+import Profile from "./components/Profile";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header is common to all components */}
+      <Header />
+      {/* Outlet */}
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+        ],
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:id",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
